598eb5312f502fd13e8cf69c90ca13e15609316b
Lucas Almeida Moreira <136464474+lucasalmo@users.noreply.github.com>
2024-08-29 15:33:15 -0300

    Update 01-Vetores.c

--- 01-Vetores.c
+++ 01-Vetores.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 int main() {
+    //comentei este vetor estático
     //int vetor[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int i, temp;
     int n;
-    
+
+    //imprime a mensagem na tela pedindo quantas posições terá o vetor 
     printf("insira a quantidade de posições do vetor: ");
     scanf("%d", &n);
+    //declaro o vetor já recebendo a quantidade digitada pelo usuário
     int vetor[n];
 
     for (i = 0; i < n; i++) {
@@ -17,6 +20,7 @@ int main() {
     }
 
     printf("Vetor invertido:\n");
+    //modificando a estrutura do for para ele começar imprimir direto da última posição para a primeira
     for (i = n-1; i >= 0; i--) {
         printf("%d ", vetor[i]);
     }
